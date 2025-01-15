6c593f7f5a9589390eb147623e252263702b6092
kamshat18 <k_baktygerey@kbtu.kz>
2025-01-15 12:10:57 +0500

    Добавил папку lab01 и связанные файлы

---
+++ lab01/01.py
@@ -0,0 +1,63 @@
+print("Hello, World!")#example from home-get started
+#PYTHON COMMENTS
+#This is a comment.
+"""
+This is a comment
+written in
+more """
+#PYTHON VARIABLES TOPIC
+x = 5 
+y = "John"
+print(x)
+print(y)
+myVariableName = "John"#camel case
+MyVariableName = "John"#pascal case
+my_variable_name = "John"#snake case
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z) #Assign Multiple Values
+x = 5
+y = 10
+print(x + y)#output variables
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc() #Global variables
+#PYTHON DATA TYPES
+x = {"name" : "John", "age" : 36}	#dict
+x = ("apple", "banana", "cherry")	#tuple
+x = 20.5	#float etc
+#PYTHON CASTING
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+#PYTHON STRINGS
+a = "Hello, World!"
+print(a[1])
+txt = "The best things in life are free!"
+print("free" in txt)
+#python sliciing string
+b = "Hello, World!"
+print(b[2:5])
+#modify strings
+a = "Hello, World!"
+print(a.upper())
+#String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+#String Format
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+# Escape Characters
+txt = "We are the so-called \"Vikings\" from the north."
+#String Methods
+txt = "Hello My Name Is PETER"
+x = txt.swapcase()
+print(x)
+#DONE!!!
