64ee2df4dc2117a0e14e690bc431843860174899
kamshat188 <kamshat.b18@mail.ru>
2025-01-27 09:02:15 +0500

    updating

--- lab01/lab02.py
+++ lab01/lab02.py
@@ -6,3 +6,25 @@ print(bool("")) ,print(bool("Hello"))
 print(6 & 2 + 1)
 print((6 + 3) - (6 + 3))
 #Python Lists
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5]) #Access Items
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist) #Change List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)# Add List Items
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist) #Remove List Items
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+  print(x)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = [x for x in fruits if "a" in x]
+print(newlist)#Python - List Comprehension
