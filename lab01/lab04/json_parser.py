5a942f82278feecc8a5cd6760a8f4df0eb2449d0
kamshat188 <kamshat.b18@mail.ru>
2025-02-12 11:52:05 +0500

    exercise 1

---
+++ lab01/lab04/json_parser.py
@@ -0,0 +1,15 @@
+import json
+
+with open("sample-data.json", "r") as file:
+    data = json.load(file)
+\
+print("Interface Status")
+print("=" * 80)
+print("{:<50} {:<20} {:<10} {:<10}".format("DN", "Description", "Speed", "MTU"))
+print("-" * 80)
+
+for item in data["imdata"]:
+    interface = item["l1PhysIf"]["attributes"]
+    print("{:<50} {:<20} {:<10} {:<10}".format(
+        interface["dn"], interface.get("descr", "inherit"), interface["speed"], interface["mtu"]
+    ))
